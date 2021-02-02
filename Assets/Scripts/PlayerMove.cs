33e83a76cb6401f5f1e7ea6b61a87d6e860b1aca
souliflame <souliflame01@gmail.com>
2021-02-02 14:39:34 +0900

    1: Add Atlas animation 2: Add character and floor

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float maxSpeed;
+    Rigidbody2D rigid;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        // Move by key control
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        if(rigid.velocity.x > maxSpeed) // Right max speed
+        {
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        }
+        else if(rigid.velocity.x < maxSpeed * -1)   // Left max speed
+        {
+            rigid.velocity = new Vector2(maxSpeed * -1, rigid.velocity.y);
+        }
+    }
+}
