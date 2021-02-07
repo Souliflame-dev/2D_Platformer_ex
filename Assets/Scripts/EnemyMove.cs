931189cb602369900349e65c84ba9e6c2b48ee3a
souliflame <souliflame01@gmail.com>
2021-02-07 23:20:10 +0900

    1: Enemy AI 2: Player Attack 3: Player Damaged(Enemy and trap) 4: Fix NullException(Trap's enemy tag causes nullException error on condition that Player transform.y lower than TileMap.transform.y)

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    Animator anim;
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    CapsuleCollider2D capsuleCollider;
+
+    public int nextMove;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+
+        Invoke("Think", 5);
+    }
+
+    void FixedUpdate()
+    {
+        //Move
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+
+        //Check Platform
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove * 0.5f, rigid.position.y);
+        Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector2.down, 1, LayerMask.GetMask("Platform"));
+        if (rayHit.collider == null)
+        {
+            Turn();
+        }
+    }
+
+    void Think()
+    {
+        //Set Next Active
+        nextMove = Random.Range(-1, 2);
+
+        //Sprite Animation
+        anim.SetInteger("WalkSpeed", nextMove);
+
+        //Flip Sprite
+        if (nextMove != 0)
+        {
+            spriteRenderer.flipX = nextMove == 1;
+        }
+
+        //Recursive
+        float nextThinkTime = Random.Range(2f, 5f);
+        Invoke("Think", nextThinkTime);
+    }
+
+    void Turn()
+    {
+        nextMove *= -1;
+        spriteRenderer.flipX = !spriteRenderer.flipX;
+
+        CancelInvoke("Think");
+        Invoke("Think", 2);
+    }
+
+    public void OnDamaged()
+    {
+        //Sprite Alpha
+        spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+
+        //Sprite Flip Y
+        spriteRenderer.flipY = true;
+
+        //Collider Disable
+        capsuleCollider.enabled = false;
+
+        //Die Effect Jump
+        rigid.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
+
+        //Destroy
+        Invoke("DeActive", 5f);
+    }
+
+    void DeActive()
+    {
+        gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,26 +5,189 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float maxSpeed;
+    public float stopSpeed;
+    public float jumpPower;
+    public GameManager gameManager;
+
     Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator animator;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //Jump
+        if (Input.GetButtonDown("Jump") && !animator.GetBool("isJumping"))
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            animator.SetBool("isJumping", true);
+        }
+
+        //Stop Speed
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * stopSpeed, rigid.velocity.y);
+        }
+
+        //Direction Sprite
+        if (Input.GetButton("Horizontal"))
+        {
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+        }
+
+        //Animation
+        if (Mathf.Abs(rigid.velocity.x) < 0.1)
+        {
+            animator.SetBool("isWalking", false);
+        }
+        else
+        {
+            animator.SetBool("isWalking", true);
+        }
     }
 
     void FixedUpdate()
     {
-        // Move by key control
+        // Move Speed
         float h = Input.GetAxisRaw("Horizontal");
-        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+        rigid.AddForce(Vector2.right * h * 2, ForceMode2D.Impulse);
 
-        if(rigid.velocity.x > maxSpeed) // Right max speed
+        //Max Speed
+        if (rigid.velocity.x > maxSpeed) // Right max speed
         {
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
         }
-        else if(rigid.velocity.x < maxSpeed * -1)   // Left max speed
+        else if (rigid.velocity.x < maxSpeed * -1)   // Left max speed
         {
             rigid.velocity = new Vector2(maxSpeed * -1, rigid.velocity.y);
         }
+
+        //Landing Platform(Raycast)
+        if (rigid.velocity.y < 0)
+        {
+            Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+            if (rayHit.collider != null)
+            {
+                if (rayHit.distance < 0.51f)
+                {
+                    Debug.Log("isJumping");
+                    animator.SetBool("isJumping", false);
+                }
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "Trap")
+        {
+            //Attack
+            if (collision.gameObject.tag == "Enemy" && (transform.position.y > collision.gameObject.transform.position.y) && rigid.velocity.y <= 0)
+            {
+                OnAttack(collision.transform);
+            }
+            else
+            {
+                OnDamaged(collision.transform.position);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Item")
+        {
+            //Point
+            bool isBronze = collision.gameObject.name.Contains("Bronze");
+            bool isSilver = collision.gameObject.name.Contains("Silver");
+            bool isGold = collision.gameObject.name.Contains("Gold");
+            if (isBronze)
+            {
+                gameManager.stagePoint += 50;
+            }
+            else if (isSilver)
+            {
+                gameManager.stagePoint += 100;
+            }
+            else if (isGold)
+            {
+                gameManager.stagePoint += 300;
+            }
+
+            //Disable Coin
+            collision.gameObject.SetActive(false);
+        }
+        else if (collision.gameObject.tag == "Finish")
+        {
+            //Next Stage
+            gameManager.NextStage();
+        }
+    }
+
+    void OnAttack(Transform enemy)
+    {
+        //Point
+        gameManager.stagePoint += 100;
+
+        //Reaction Force
+        rigid.AddForce(Vector2.up * 3, ForceMode2D.Impulse);
+
+        //Enemy Die
+        EnemyMove enemyMove = enemy.GetComponent<EnemyMove>();
+        enemyMove.OnDamaged();
+    }
+
+    void OnDamaged(Vector2 targetPos)
+    {
+        //Health Down
+        gameManager.HealthDown();
+
+        //Change Layer(Immortal Active)
+        gameObject.layer = 11;
+
+        //Immortal Effect
+        spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+
+        //Reaction Force
+        int dirc = transform.position.x - targetPos.x > 0 ? 1 : -1;
+        rigid.AddForce(new Vector2(dirc, 1) * 10, ForceMode2D.Impulse);
+
+        //Animation
+        animator.SetTrigger("doDamaged");
+
+        Invoke("OffDamaged", 0.8f);
+    }
+
+    void OffDamaged()
+    {
+        //Change Layer(Normal)
+        gameObject.layer = 10;
+
+        spriteRenderer.color = new Color(1, 1, 1, 1);
+    }
+
+    public void OnDie()
+    {
+        //Sprite Alpha
+        spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+
+        //Sprite Flip Y
+        spriteRenderer.flipY = true;
+
+        //Collider Disable
+        GetComponent<CapsuleCollider2D>().enabled = false;
+
+        //Die Effect Jump
+        rigid.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
+
     }
 }
